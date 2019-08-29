e265d546ea173eaca4d3896ca7515e12e0c13f1f
richardhendricksen <richard.hendricksen@gmail.com>
2019-08-29 10:43:13 +0200

    improved buildDockerImage script, preparing for travis build

--- scripts/buildDockerImage.sh
+++ scripts/buildDockerImage.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 function help_text {
     cat <<EOF
-    Usage: $0 [ -p|--profile PROFILE ] [-h]
+    Usage: $0 [ -i|--aws_account_id AWS_ACCOUNT_ID ] [ -r|--region ECR_REGION] [ -p|--profile PROFILE ] [-h]
 
-        --profile PROFILE               (optional) The profile to use from ~/.aws/credentials.
+        --aws_account_id AWS_ACCOUNT_ID   (required) AWS account id
+        --region REGION                   (required) Region of ECR repository
+        --profile PROFILE                 (optional) The profile to use from ~/.aws/credentials.
 
 EOF
     exit 1
@@ -14,6 +16,14 @@ while [ $# -gt 0 ]; do
     case $arg in
         -h|--help)
             help_text
+        ;;
+        -i|--aws_account_id)
+            export AWS_ACCOUNT_ID="$2"
+            shift; shift
+        ;;
+        -r|--region)
+            export ECR_REGION="$2"
+            shift; shift
         ;;
         -p|--profile)
             export AWS_DEFAULT_PROFILE="$2"
@@ -27,9 +37,19 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-# Set ECR vars
-export ECR_ID=12345
-export ECR_REGION=eu-west-1
+if [ -z "$ECR_REGION" ]
+then
+    echo "ECR region required."
+    help_text
+    exit 1
+fi
+
+if [ -z "$AWS_ACCOUNT_ID" ]
+then
+    echo "AWS Account id required."
+    help_text
+    exit 1
+fi
 
 # Determine script dir
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -40,6 +60,6 @@ docker build -t gatling-runner .
 
 # Push docker image to AWS:
 $(aws ecr get-login --no-include-email)
-docker tag gatling-runner ${ECR_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/bingel/gatling-runner
-docker push ${ECR_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/bingel/gatling-runner
-docker logout ${ECR_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/bingel/gatling-runner
+docker tag gatling-runner ${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/gatling-runner
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/gatling-runner
+docker logout ${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/gatling-runner
