aa31c1a5f806d33cf7b981211542ff4b17bb05ae
richardhendricksen <richard.hendricksen@gmail.com>
2020-07-15 14:45:09 +0200

    fixed run.sh script

--- gatling-runner/bin/run.sh
+++ gatling-runner/bin/run.sh
@@ -7,7 +7,7 @@
 USER_ARGS=""
 COMPILATION_CLASSPATH=`find -L ./target -maxdepth 1 -name "*.jar" -type f -exec printf :{} ';'`
 JAVA_OPTS="-server -Xmx1G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:+HeapDumpOnOutOfMemoryError -XX:MaxInlineLevel=20 -XX:MaxTrivialSize=12 -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false ${JAVA_OPTS}"
-java $JAVA_OPTS $USER_ARGS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling -s ${SIMULATION}
+java $JAVA_OPTS $USER_ARGS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling -s $SIMULATION
 
 # Upload simulation.log to S3
 for _dir in results/*/
